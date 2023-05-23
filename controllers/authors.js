7e94d082c704daed979d85e354b9fe51cb104e4b
Monica Sue <monicasue567@gmail.com>
2023-05-23 14:49:59 -0400

    add routes, controller functions and test for create, index, and show

--- controllers/authors.js
+++ controllers/authors.js
@@ -9,6 +9,27 @@ const create = async (req, res) => {
   }
 }
 
+const index = async (req, res) => {
+  try {
+    const authors = await Author.findAll()
+    res.status(200).json(authors)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+const show = async (req, res) => {
+  try {
+    const author = await Author.findByPk(req.params.id)
+    res.status(200).json(author)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
 module.exports = {
   create,
+  index,
+  show,
+
 }
--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const authorsCtrl = require('../controllers/authors.js')
 
+router.get('/', authorsCtrl.index)
+router.get('/:id', authorsCtrl.show)
 router.post('/', authorsCtrl.create)
 
 module.exports = router
