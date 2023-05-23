e2fa756937c5bd7d6818b1a3ee9dbf7151242702
Monica Sue <monicasue567@gmail.com>
2023-05-23 19:58:05 -0400

    Parts 1 and 2 complete

---
+++ seeders/20230523235103-author.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    const date = new Date()
+
+    await queryInterface.bulkInsert('Authors', [{
+      name: 'WM. Paul Young',
+      nationality: 'Canadian',
+      genre: 'Spirituality, Fiction',
+      createdAt: date,
+      updatedAt: date,
+    }])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Authors', null, {})
+  }
+};
